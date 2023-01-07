35c2071ef5bb32630eee4533fddb85593a0a8463
Zixuan Wang <zxwang42@gmail.com>
2023-01-07 08:10:36 +0000

    Bump to 1.0.3

---
+++ scripts/draft_new_release.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+read -p "Input the new release version (without the leading 'v'): " version
+
+jq ".version = \"$version\"" package.json > package.json
+git add package.json
+
+msg="Bump to $version"
+git commit -m "$msg"
+git tag -a "v$version" -m "$msg"
